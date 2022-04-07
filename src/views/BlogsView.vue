706a33aff0b16c9b17b7bb6e30d34ef7cf90a654
Vicencio42 <vicencio42@gmail.com>
2022-04-06 21:36:29 -0500

    added signup, login, logout to frontend vuejs

---
+++ src/views/BlogsView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="blogs">
+    <h1>Let Blog Some Stuff!!!</h1>
+    <img src="https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2017/01/ebay_troll.jpg" />
+  </div>
+</template>
