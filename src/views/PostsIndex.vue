d165d563f87f935c3d980088feee5330733e9ebe
Vicencio42 <vicencio42@gmail.com>
2022-04-12 20:11:08 -0500

    addded cards to posts, adjusted imagepixles

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="home">
-    <h1>{{ message }} count: {{ posts.length }}</h1>
-    <div v-for="post in posts" v-bind:key="post.id">
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.body }}</p>
-      <img v-bind:src="post.image" v-bind:alt="post.title" />
-      <br />
-      <router-link v-bind:to="`/posts/${post.id}`">Show Info</router-link>
+  <div class="home row">
+    <h1 v-bind:class="className">{{ message }} count: {{ posts.length }}</h1>
+    <div class="col" v-for="post in posts" v-bind:key="post.id">
+      <div class="card" style="width: 18rem"></div>
+      <img v-bind:src="post.image" class="card-img-top" v-bind:key="post.id" />
+      <div class="card-body">
+        <h2 class="card-title">{{ post.title }}</h2>
+        <p class="card-body">{{ post.body }}</p>
+        <!-- <img v-bind:src="post.image" v-bind:alt="post.title" /> -->
+        <br />
+        <router-link v-bind:to="`/posts/${post.id}`" class="btn btn-primary">Show Info</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -33,3 +37,10 @@ export default {
   },
 };
 </script>
+<style>
+img {
+  height: 450px;
+  width: 250px;
+  object-fit: cover;
+}
+</style>
